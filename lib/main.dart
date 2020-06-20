2444b31373154e1a65d5993fdbee34c0f6408f6e
roseline124 <guseod24@gmail.com>
2020-06-20 14:56:33 +0900

    Add QuestionFive screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/HexColor.dart';
 import './screens/homeScreen/homeScreen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class DreamBodyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Color.fromRGBO(127, 102, 255, 1),
-        accentColor: Colors.orangeAccent,
+        accentColor: HexColor('#ffcc00'),
       ),
       title: 'Dream Body',
       home: HomeScreen(),
