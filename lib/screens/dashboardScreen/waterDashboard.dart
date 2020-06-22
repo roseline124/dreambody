f7bd740d68a6bf437d8e82c7fba5ebf83697e58b
roseline124 <guseod24@gmail.com>
2020-06-23 00:31:33 +0900

    Add WaterIndicator with opacity

--- lib/screens/dashboardScreen/waterDashboard.dart
+++ lib/screens/dashboardScreen/waterDashboard.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
-class WaterDashboard extends StatelessWidget {
+class WaterDashboard extends StatefulWidget {
   const WaterDashboard({Key key}) : super(key: key);
 
   @override
+  _WaterDashboardState createState() => _WaterDashboardState();
+}
+
+class _WaterDashboardState extends State<WaterDashboard> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Opacity(
-      opacity: 0.3,
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.purple, Colors.blue])),
+      child: Stack(
+        children: [
+          // WaterIndicator(progress: 0.3, progressColor: Colors.amber),
+          WaterIndicator(progress: 0.35, progressColor: Colors.amber[100]),
+        ],
+      ),
+    ));
+  }
+}
+
+class WaterIndicator extends StatelessWidget {
+  const WaterIndicator({
+    this.progressColor,
+    this.borderColor = Colors.transparent,
+    this.progress = 0.5,
+    this.width = 200,
+    this.height = 200,
+    this.opacity = 0.3,
+  });
+
+  final double width;
+  final double height;
+  final double opacity;
+  final double progress;
+  final Color borderColor;
+  final Color progressColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: opacity,
       child: Center(
         child: SizedBox(
-          width: 200,
-          height: 200,
+          width: width,
+          height: height,
           child: LiquidCircularProgressIndicator(
-            value: 0.25,
-            valueColor: AlwaysStoppedAnimation(Colors.pink),
-            backgroundColor: Colors.white,
-            borderColor: Colors.red,
+            value: progress,
+            valueColor: AlwaysStoppedAnimation(progressColor),
+            backgroundColor: Colors.transparent,
+            borderColor: borderColor,
             borderWidth: 1.0,
             direction: Axis.vertical,
           ),
         ),
       ),
-    ));
+    );
   }
 }
