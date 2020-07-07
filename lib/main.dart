513402f5b121196280a6dd744e93ab0e32ee75ec
roseline124 <guseod24@gmail.com>
2020-07-08 00:07:27 +0900

    Add graphql provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'widgets/hexColor.dart';
-import './screens/homeScreen/homeScreen.dart';
+import 'services/graphqlService.dart';
 import 'package:dreambody/screens/typeSelectionScreen/typeSelection.dart';
+import './screens/homeScreen/homeScreen.dart';
 import './screens/dashboardScreen/waterDashboard.dart';
 import './screens/signInScreen/signIn.dart';
 
@@ -13,19 +15,23 @@ void main() {
 class DreamBodyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(127, 102, 255, 1),
-        accentColor: HexColor('#ffcc00'),
-      ),
-      title: 'Dream Body',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/login': (context) => SignInScreen(),
-        '/questions': (context) => TypeSelection(),
-        '/water': (context) => WaterDashboard()
-      },
+    return GraphQLProvider(
+      client: graphqlService.client,
+      child: CacheProvider(
+          child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(127, 102, 255, 1),
+          accentColor: HexColor('#ffcc00'),
+        ),
+        title: 'Dream Body',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(),
+          '/login': (context) => SignInScreen(),
+          '/questions': (context) => TypeSelection(),
+          '/water': (context) => WaterDashboard()
+        },
+      )),
     );
   }
 }
