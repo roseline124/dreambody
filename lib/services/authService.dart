25d3826978ebc707793f49e2f286ceccfbcbd6bd
roseline124 <guseod24@gmail.com>
2020-07-11 14:12:10 +0900

    Fix authService

--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,23 +1,14 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert' as JSON;
-
 class AuthService {
-  Future<String> _token;
+  String _token;
 
   getToken() {
     if (_token == null) {
-      _token = _getToken();
+      _token = saveToken();
     }
     return _token;
   }
 
-  refresh() {
-    _token = _getToken();
-  }
-
-  _getToken() async {
-    final res = await http.post('blabla');
-    final body = JSON.jsonDecode(res.body);
-    return body['token'];
+  saveToken({token: String}) {
+    _token = token;
   }
 }
